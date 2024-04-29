9d51affdd132d6684a3b5133bf5d590c97e14ed3
unknown <ragimshin@gmail.com>
2024-04-29 21:00:34 +0300

    added test requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+
+  async function testGetReq() {
+    const response = await axios.get('/auth/register')
+    console.log(response.data)
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={testGetReq}>
+        get
+      </button>
     </div>
   );
 }
