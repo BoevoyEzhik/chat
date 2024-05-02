c3328e6e4eaf99dd66a72914f0ca27738e60be14
BoevoyEzhik <nikita120597@gmail.com>
2024-05-02 22:08:00 +0300

    поправил импоррты

--- backend/db/session_maker.py
+++ backend/db/session_maker.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from backend.db.db_config import db_url, local_db_url
+from db_config import db_url, local_db_url
 
 
 engine = create_engine(db_url, echo=True)
