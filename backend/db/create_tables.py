cb4534521000de2fab321dbb1fc3b3364f699792
BoevoyEzhik <nikita120597@gmail.com>
2024-05-03 00:08:08 +0300

    пофиксил бд

--- backend/db/create_tables.py
+++ backend/db/create_tables.py
@@ -1,18 +1,16 @@
-from models.model_user import Base
-from session_maker import drop_create_engine
+from backend.db.models.model_user import Base
+from backend.db.session_maker import engine
 
 
 def create_tables():
-    Base.metadata.create_all(bind=drop_create_engine)
+    Base.metadata.create_all(bind=engine)
 
 
 def drop_tables():
-    Base.metadata.drop_all(bind=drop_create_engine)
+    Base.metadata.drop_all(bind=engine)
 
 
 def drop_create():
     drop_tables()
     create_tables()
 
-
-drop_create()
--- backend/db/session_maker.py
+++ backend/db/session_maker.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from db_config import db_url, local_db_url
+from backend.db.db_config import db_url
 
 
 engine = create_engine(db_url, echo=True)
 Session = sessionmaker(bind=engine)
-drop_create_engine = create_engine(local_db_url, echo=True)
